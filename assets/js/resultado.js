f8761ca1847f636023b05a8697e7cb157ebec363
facundo <142283126+fcu2134@users.noreply.github.com>
2023-11-11 19:35:41 -0300

    resultado.js

--- assets/js/resultado.js
+++ assets/js/resultado.js
@@ -10,7 +10,7 @@ const nota6 = parseFloat(urlParams.get("nota6"));
 
 
 const ponderacionUnidad1 = (nota1 * 0.12  + nota2 * 0.21).toFixed(1);
-const ponderacionUnidad2 = (nota3 * 0.21  + nota4 * 0.21) .toFixed(1);
+const ponderacionUnidad2 = (nota3 * 0.12  + nota4 * 0.21) .toFixed(1);
 const ponderacionUnidad3 = (nota5 * 0.12 + nota6 * 0.21  ).toFixed(1);
 
 const promedioPonderado = (parseFloat(ponderacionUnidad1) + parseFloat(ponderacionUnidad2) + parseFloat(ponderacionUnidad3)).toFixed(2);
